85456442c3decbd0d6638cfe1e8e67dae5fac063
wongchc <chris-i.wong@ubs.com>
2015-05-26 18:13:39 +0100

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-school
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clojure-school "0.1.0-SNAPSHOT"
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler liberator-example/handler}
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+  	[org.clojure/clojure "1.6.0"]
+  	[liberator "0.13"]
+    [compojure "1.1.3"]
+    [ring/ring-core "1.2.1"]
+  ])
